402943459528b43fe731b6d9b0f48eac041590c7
dwiprogrammer <ini.dwiii@gmail.com>
2020-01-26 14:40:25 +0800

    Menambahkan file Config.php dan memodifikasi file App.php dan menambahkan beberapa fitur serta menambahkan beberapa penjelasan

--- app/core/App.php
+++ app/core/App.php
@@ -2,17 +2,111 @@
 
 class App
 {
+    /**
+     * 
+     * Deklarasi variable controller, method, dan parameter default
+     * 
+     * @param String $controller, type data controller harus bernilai string
+     * @param String $method, type data method harus bernilai string. Default adalah 'index'
+     * @param array $params, type data params bernilai array karena akan menampung banyak value
+     * 
+     */
+    private $controller = DEFAULT_HOMEPAGE, $method = "index", $params = [];
+
     public function __construct()
     {
-        $url = $this->parseURL();
-        var_dump($url);
+        $url = $this->parseURL(); // diambil dari method pemecah url
+
+        /**
+         * 
+         * Memanggil controller
+         * 
+         * dibawah ini merupakan teknik memecah url dan mengambil datanya untuk digunakan
+         * dalam memanggil controller. Jika url kosong maka controller default akan
+         * dipanggil
+         * 
+         */
+        if (isset($url[0])) {
+            /**
+             * 
+             * file_exists($path)
+             * 
+             * mengecek eksistensi file berdasarkan path
+             * @param String $path, type data path berupa string yang merujuk pada file yg dituju
+             * 
+             */
+            if (file_exists("../app/controllers/" . $url[0] . ".php")) {
+                $this->controller = $url[0]; // redeclare default controller dengan url[0]
+                unset($url[0]); // menghilangkan url[0] setelah menredeclare default controller
+            }
+        }
+        require_once "../app/controllers/$this->controller.php"; // memanggil file controller
+        $this->controller = new $this->controller; // instansiasi controller
+
+        /**
+         * 
+         * Memanggil method
+         * 
+         * dibawah ini merupakan cara untuk mengecek method dari controller/class yang dipanggil
+         * apabila ada, maka redeclare default method. Jika tidak maka akan digunakan method default
+         * yaitu 'index'
+         * 
+         */
+        if (isset($url[1])) {
+            /**
+             * 
+             * method_exist($object, $method)
+             * 
+             * @param Object|Class $object, type data harus berupa instansiasi class/object
+             * @param String $method, type data harus string
+             * @return boolean , mengembalikan nilai berupa boolean
+             * 
+             */
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        /**
+         * 
+         * Menyimpan parameter
+         * 
+         * jika url masih memiliki value atau url tidak kosong maka gunakan value tersebut
+         * untuk digunakan sebagai data parameter, jika kosong maka parameter akan diset sebagai
+         * array kosong
+         * 
+         */
+        if (!empty($url)) {
+            $this->params = array_values($url);
+        }
+
+        /**
+         * 
+         * call_user_func_array($callback, $parameter)
+         * 
+         * method built in php untuk membuat satuan array menjadi parameter sebuah function
+         * @param callable|array|String $callback, dapat berupa array atau pun string
+         * @param array $parameter, type data array untuk dijadikan parameter dari sebuah function
+         * @return mixed , mengembalikan nilai dari function / callback yang dipanggil
+         * 
+         */
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    // method untuk memecah URL
+    /**
+     * 
+     * function parseURL()
+     * 
+     * method ini digunakan untuk mengambil url, dan memecahnya menjadi array
+     * @return array $url, method ini mengembalikan nilai array
+     * 
+     */
     public function parseURL()
     {
         // cek $_GET['url]
         if (isset($_GET['url'])) {
+            // menghilangkan spasi dibelakang url
             $url = rtrim($_GET['url']);
             // mencegah memasukkan string yang tidak semestinya
             $url = filter_var($url, FILTER_SANITIZE_URL);
---
+++ app/core/Config.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * 
+ * Config.php
+ * 
+ * gunakan file ini untuk mensetting data default untuk menjalankan website ini
+ * 
+ */
+
+/**
+ * 
+ * PAGE CONFIGURATION
+ * 
+ * dibawah ini adalah konfigurasi untuk website ini
+ * 
+ */
+
+/**
+ * 
+ * BASE_URL
+ * 
+ * variable ini digunakan untuk menyimpan base url atau url default, rubahlah dan
+ * sesuaikan dengan url localhost mu, defaultnya adalah 'http://localhost/project_inventaris/public/'
+ * 
+ */
+define('BASE_URL', 'http://localhost/project_inventaris/public/');
+
+
+/**
+ * 
+ * DEFAULT_HOMEPAGE
+ * 
+ * default page yang diload pertama kali atau jika url yang ditulis tidak memiliki kecocokan dengan
+ * controller atau method yang ada. Defaultnya adalah 'home', ubahlah sesuai dengan controller yang
+ * ingin kalian load
+ * 
+ */
+define('DEFAULT_HOMEPAGE', 'home');
+
+/**
+ * 
+ * DATABASE CONFIGURATION
+ * 
+ * dibawah ini berisi konfigurasi database, jika database kalian tidak sama dengan default database
+ * yang disediakan, kalian bisa mengganti nama atau data-data dibawah ini sesuai dengan data dari
+ * database kalian
+ * 
+ * didalam folder ini sudah disediakan default database berupa file sql yang bernama db_inventaris.sql
+ * jika ingin menggunakan nya bisa kalian import ke dalam sql_server atau DBMS kalian
+ * 
+ */
+
+/**
+ * 
+ * DB_HOST
+ * 
+ * deklarasi konstanta untuk menyimpan nama host, default adalah 'localhost'
+ * 
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ * 
+ * DB_USER
+ * 
+ * deklarasi konstanta untuk menyimpan nama user, default adalah 'root'
+ * 
+ */
+define('DB_USER', 'root');
+
+/**
+ * 
+ * DB_PASS
+ * 
+ * deklarasi untuk menyimpan password dari sql_server, default adalah '' (string kosong)
+ * 
+ */
+define('DB_PASS', '');
+
+/**
+ * 
+ * DB_NAME
+ * 
+ * deklarasi untuk menyimpan nama database yang akan digunakan, default adalah 'db_inventaris'
+ * 
+ */
+define('DB_NAME', 'db_inventaris');
